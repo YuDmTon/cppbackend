af1d474ac0582baa658fdb4ff3592daa0186ac55
YuTon <y.tonevitskiy@labinsys.ru>
2024-10-19 11:53:01 +0400

    serialization 1st commit

---
+++ sprint4/problems/state_serialization/solution/src/app.h
@@ -0,0 +1,189 @@
+#pragma once
+
+#include <boost/json.hpp>
+
+#include <algorithm>
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <optional>
+#include <random>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "model.h"
+
+namespace app {
+
+using namespace std::literals;
+
+namespace json = boost::json;
+namespace fs   = std::filesystem;
+
+
+//// Player ///////////////////////////////////////////////////////////////////////////////////////
+class Player {
+public:
+    Player(model::Dog* dog, const model::GameSession* session)
+        : dog_(dog)
+        , session_(session)
+        , dog_id_(0)
+        , session_id_("")
+    {
+        if ( dog_ != nullptr ) {
+            dog_id_ = dog->GetId();
+        }
+        if ( session_ != nullptr ) {
+            session_id_ = session->GetMapId();
+        }
+    }
+    model::Dog* GetDog() const noexcept { return dog_; }
+    const model::GameSession* GetSession() const noexcept { return session_; }
+    // for deserialization only
+    uint32_t GetDogId() const noexcept { return dog_id_; }
+    model::Map::Id GetSessionId() const noexcept { return session_id_; }
+    void SetDogId(uint32_t dog_id) { dog_id_ = dog_id; }
+    void SetSessionId(model::Map::Id session_id) { session_id_ = session_id; }
+    //
+    std::string ToString(std::string offs) const;
+
+private:
+    model::Dog*               dog_;
+    const model::GameSession* session_;
+    // for deserialization only
+    uint32_t                  dog_id_;
+    model::Map::Id            session_id_;
+};  // Player
+
+
+//// PlayerToken Generator ////////////////////////////////////////////////////////////////////////
+class PlayerToken {
+    constexpr static size_t TOKEN_SIZE = 32;
+public:
+    PlayerToken() = default;
+    std::string Get() {
+        std::ostringstream oss;
+        oss << std::hex << generator1_() << generator2_();
+        std::string token = oss.str();
+        while ( token.size() < TOKEN_SIZE ) {
+            token = "0" + token;
+        }
+        return token;
+    }
+private:
+    std::random_device random_device_;
+    std::mt19937_64 generator1_{[this] {
+        std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+        return dist(random_device_);
+    }()};
+    std::mt19937_64 generator2_{[this] {
+        std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+        return dist(random_device_);
+    }()};
+};  // PlayerToken
+
+
+//// Players //////////////////////////////////////////////////////////////////////////////////////
+class Players {
+public:
+    Players() = default;
+    std::string Add(model::Dog* dog, const model::GameSession* session) {
+        std::string  token = PlayerToken().Get();
+        const size_t index = players_.size();
+        Player player(dog, session);
+        players_.push_back(player);
+        //
+        token_to_index_[token] = index;
+        //
+        return token;
+    }
+    bool HasToken(std::string token) const noexcept {
+        return token_to_index_.find(token) != token_to_index_.end();
+    }
+    Player* FindByToken(std::string token) noexcept {
+        if ( HasToken(token) ) {
+            return &players_.at(token_to_index_.at(token));
+        }
+        return nullptr;
+    }
+    const std::vector<Player>& GetPlayers() const noexcept { return players_; }
+    // for deserialization only
+    const std::unordered_map<std::string, size_t>  GetPlayersTokenToIndex() const noexcept { return token_to_index_; }
+    Player* AddPlayer(Player player) {
+        players_.push_back(player);
+        return &players_.back();
+    }
+    void AddPlayersTokenAndIndex(std::string token, size_t index) { token_to_index_[token] = index; }
+    //
+    std::string ToString() const;
+
+private:
+    std::string DebugToken() {
+        const int   TOKEN_SIZE = 32;
+        std::string debug_token(TOKEN_SIZE, static_cast<char>(0x30 + players_.size()));
+        return debug_token;
+    }
+
+private:
+    std::vector<Player> players_;
+    std::unordered_map<std::string, size_t> token_to_index_;
+};  // Players
+
+
+//// Application //////////////////////////////////////////////////////////////////////////////////
+class Application {
+public:
+    explicit Application(model::Game& game, bool debug_mode, bool randomize_spawn, std::string state_file, uint32_t save_period)
+            : game_(game)
+            , debug_mode_(debug_mode)
+            , randomize_spawn_(randomize_spawn)
+            , state_file_(state_file)
+            , save_period_(save_period)
+            , dog_id_(0)
+            , curr_time_(0)
+            , save_time_(0) {
+    }
+
+    // unauthorized
+    std::optional<std::string> GetMap(const std::string& map_id);
+    bool GetMaps(std::string& res_body);
+    bool TryJoin(const std::string& user_name, const std::string& map_id, std::string& res_body);
+    // authorized
+    bool GetPlayers(const std::string& token, std::string& res_body);
+    bool GetState(const std::string& token, std::string& res_body);
+    bool Move(const std::string& token, const std::string& move, std::string& res_body);
+    // debug (unauthorized)
+    std::string Tick(uint32_t time_delta);
+    // for deserialization only
+    std::string GetStateFile() const noexcept { return state_file_; }
+    //
+    model::Game& GetGame() const noexcept { return game_; }
+    const Players& GetPlayers() const noexcept { return players_; }
+    uint32_t GetDogId() const noexcept { return dog_id_; }
+    //
+    Player* AddPlayer(Player player) { return players_.AddPlayer(player); }
+    void AddPlayersTokenAndIndex(std::string token, size_t index) { players_.AddPlayersTokenAndIndex(token, index); }
+    void SetDogId(uint32_t dog_id) { dog_id_ = dog_id; }
+    //
+    std::string ToString() const;
+
+private:
+    // components
+    model::Game& game_;
+    Players      players_;
+    // command line arguements
+    bool         debug_mode_;
+    bool         randomize_spawn_;
+    std::string  state_file_;
+    uint32_t     save_period_;
+    // work variables
+    uint32_t     dog_id_;
+    uint64_t     curr_time_;
+    uint64_t     save_time_;
+};  // Application
+
+}   // namespace app
---
+++ sprint4/problems/state_serialization/solution/src/serializer.h
@@ -0,0 +1,295 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <boost/serialization/deque.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/unordered_map.hpp>
+
+#include "app.h"
+#include "model.h"
+
+namespace geom {
+
+template <typename Archive>
+void serialize(Archive& ar, Point2D& point, [[maybe_unused]] const unsigned version) {
+    ar& point.x;
+    ar& point.y;
+}
+
+template <typename Archive>
+void serialize(Archive& ar, Vec2D& vec, [[maybe_unused]] const unsigned version) {
+    ar& vec.x;
+    ar& vec.y;
+}
+
+}  // namespace geom
+
+namespace model {
+
+template <typename Archive>
+void serialize(Archive& ar, Position& obj, [[maybe_unused]] const unsigned version) {
+    ar & obj.x;
+    ar & obj.y;
+}
+
+template <typename Archive>
+void serialize(Archive& ar, Speed& obj, [[maybe_unused]] const unsigned version) {
+    ar & obj.sx;
+    ar & obj.sy;
+}
+
+template <typename Archive>
+void serialize(Archive& ar, BagItem& obj, [[maybe_unused]] const unsigned version) {
+    ar & obj.id_;
+    ar & obj.type_;
+}
+
+template <typename Archive>
+void serialize(Archive& ar, LostObject& obj, [[maybe_unused]] const unsigned version) {
+    ar & obj.id_;
+    ar & obj.type_;
+    ar & obj.position_;
+    ar & obj.CURR_ID;
+}
+
+}  // namespace model
+
+namespace app {
+
+}  // namespace app
+
+
+
+
+namespace serialization {
+
+//// model !!! //////////////////////////////////////////////////////////
+// DogRepr (DogRepresentation) - сериализованное представление класса Dog
+class DogRepr {
+public:
+    DogRepr() = default;
+
+    explicit DogRepr(const model::Dog& dog)
+        : name_(dog.GetName())
+        , id_(dog.GetId())
+        //
+        , pos_(dog.GetPosition())
+        , start_pos_(dog.GetStartPos())
+        , speed_(dog.GetSpeed())
+        , dir_(dog.GetDirection())
+        //
+        , bag_(dog.GetBag())
+        , bag_capacity_(dog.GetBagCapacity())
+        , score_(dog.GetScore())
+        , value_(dog.GetValue())
+    { }
+
+    [[nodiscard]] model::Dog Restore() const {
+        model::Dog dog{name_, id_};
+        //
+        dog.SetPosition(pos_);
+        dog.SetStartPos(start_pos_);
+        dog.SetSpeed(speed_);
+        dog.SetDirection(dir_);
+        //
+        dog.SetBagCapacity(bag_capacity_);
+        for (const auto& bag_item : bag_) {
+            if (!dog.PushIntoBag(bag_item, 0)) {
+                throw std::runtime_error("Failed to put bag content");
+            }
+        }
+        //
+        dog.SetScore(score_);
+        dog.SetValue(value_);
+        //
+        return dog;
+    }
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+        ar & name_;
+        ar & id_;
+        //
+        ar & pos_;
+        ar & start_pos_;
+        ar & speed_;
+        ar & dir_;
+        //
+        ar & bag_;
+        ar & bag_capacity_;
+        ar & score_;
+        ar & value_;
+    }
+
+    std::string GetName() const { return name_; }
+    unsigned GetId() const { return id_; }
+
+private:
+    std::string      name_;
+    unsigned         id_ = 0;
+    //
+    model::Position  pos_;
+    model::Position  start_pos_;
+    model::Speed     speed_;
+    model::Direction dir_ = model::Direction::NORTH;
+    model::Bag       bag_;
+    size_t           bag_capacity_ = 0;
+    unsigned         score_ = 0;
+    unsigned         value_ = 0;
+};
+//////
+
+
+// GameSessionRepr (GameSessionRepresentation) - сериализованное представление класса GameSession
+class GameSessionRepr {
+public:
+    GameSessionRepr() = default;
+
+    explicit GameSessionRepr(const model::GameSession& session)
+        : lost_objects_(session.GetLostObjects())
+        , map_id_(session.GetMapId())
+    {
+        for (const auto& dog : session.GetDogs()) {
+            dogs_repr_.push_back(DogRepr(dog));
+        }
+    }
+
+    void Restore(model::GameSession* session) const {
+        for (const auto& dog_repr : dogs_repr_) {
+            session->AddDog(dog_repr.Restore());
+        }
+        for (const auto& lost_object : lost_objects_) {
+            session->AddLostObject(lost_object);
+        }
+    }
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+        ar & dogs_repr_;
+        ar & lost_objects_;
+        ar & *map_id_;
+    }
+
+    model::Map::Id GetSessionId() const noexcept { return map_id_; }
+
+private:
+    std::vector<DogRepr>           dogs_repr_;
+    std::vector<model::LostObject> lost_objects_;
+    model::Map::Id                 map_id_ = model::Map::Id{""};
+};
+
+
+//// app !!! ////////////////////////////////////////////////////////////
+// PlayerRepr (PlayerRepresentation) - сериализованное представление класса Player
+class PlayerRepr {
+public:
+    PlayerRepr() = default;
+
+    explicit PlayerRepr(const app::Player& player)
+        : dog_id_(player.GetDogId())
+        , session_id_(player.GetSessionId())
+    { }
+
+    [[nodiscard]] app::Player Restore() const {
+        app::Player player{nullptr, nullptr};
+        //
+        player.SetDogId(dog_id_);
+        player.SetSessionId(session_id_);
+        return player;
+    }
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+        ar & dog_id_;
+        ar & *session_id_;
+    }
+
+    uint32_t GetDogId() const noexcept { return dog_id_; }
+    model::Map::Id GetSessionId() const noexcept { return session_id_; }
+
+private:
+    uint32_t       dog_id_;
+    model::Map::Id session_id_ = model::Map::Id{""};
+};
+//////
+
+// AppRepr (AppRepresentation) - сериализованное представление класса Application
+class AppRepr {
+public:
+    AppRepr() = default;
+
+    explicit AppRepr(const app::Application& app) : dog_id_(app.GetDogId()) {
+        for (const auto& session : app.GetGame().GetSessions()) {
+            sessions_repr_.push_back(GameSessionRepr(session));
+        }
+        for (const auto& player : app.GetPlayers().GetPlayers()) {
+            players_repr_.push_back(PlayerRepr(player));
+        }
+        for (const auto [token, index] : app.GetPlayers().GetPlayersTokenToIndex()) {
+            token_to_index_[token] = index;
+        }
+    }
+
+    void Restore(app::Application& app) const {
+        model::Game& game = app.GetGame();
+        app.SetDogId(dog_id_);
+        for (const auto& session_repr : sessions_repr_) {
+            const model::Map* map = game.FindMap(session_repr.GetSessionId());
+            if ( map == nullptr ) {
+                std::string err = "Session: can't find map with id " + *session_repr.GetSessionId();
+                throw std::runtime_error(err);
+            }
+            model::GameSession* session = game.AddSession(map);
+            session_repr.Restore(session);
+        }
+        for (const auto& player_repr : players_repr_) {
+            model::Dog* dog = game.FindDog(player_repr.GetDogId());
+            if ( dog == nullptr ) {
+                std::string err = "Player: can't find dog with id " + *player_repr.GetSessionId();
+                throw std::runtime_error(err);
+            }
+            model::GameSession* session = game.FindSession(player_repr.GetSessionId());
+            if ( session == nullptr ) {
+                std::string err = "Player: can't find session with id " + *player_repr.GetSessionId();
+                throw std::runtime_error(err);
+            }
+            app::Player player(dog, session);
+            player.SetDogId(player_repr.GetDogId());
+            player.SetSessionId(player_repr.GetSessionId());
+            app.AddPlayer(player);
+        }
+        for (const auto [token, index] : token_to_index_) {
+            app.AddPlayersTokenAndIndex(token, index);
+        }
+    }
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+        ar & sessions_repr_;
+        ar & players_repr_;
+        ar & token_to_index_;
+        ar & dog_id_;
+    }
+
+private:
+    std::vector<GameSessionRepr>            sessions_repr_;
+    std::vector<PlayerRepr>                 players_repr_;
+    std::unordered_map<std::string, size_t> token_to_index_;
+    uint32_t                                dog_id_;
+};
+//////
+
+
+void SaveApp(app::Application& app);
+void RestoreApp(app::Application& app);
+
+void TestVectorDogsReps(std::ostream& os, std::string file_name);
+void TestPlayersReps(const app::Application& app, std::ostream& os, const std::string file_name);
+
+}  // namespace serialization
